53623a4d85834c81e0234eee8245ef64bd798eb0
marjorie <marjorie.ngoupende@gmail.com>
2022-06-07 20:57:31 +0200

    first commit

---
+++ GRAPHQL/apolloclient-example/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h2>My first Apollo app 🚀</h2>
+//     </div>
+//   );
+// }
+
+// export default App;
+
